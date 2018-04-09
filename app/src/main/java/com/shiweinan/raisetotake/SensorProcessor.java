c0bd069ba69c7ca1a0b270dec92a0d9359934680
zsyzgu <zsyzgu@163.com>
2018-04-09 21:48:46 +0800

    Add a threshold method to detect the RaiseAndTake gesture based on Angular data.

--- app/src/main/java/com/shiweinan/raisetotake/SensorProcessor.java
+++ app/src/main/java/com/shiweinan/raisetotake/SensorProcessor.java
@@ -6,6 +6,7 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Deque;
 import java.util.LinkedList;
 import java.util.List;
@@ -58,9 +59,9 @@ public class SensorProcessor implements SensorEventListener {
         double y = Math.toDegrees(values[2]);
         double z = Math.toDegrees(values[0]);
 
-        if (data.size() > 0) { //save data at least every 10ms
+        if (data.size() > 0) { //save data at least every 1ms
             long lastTime = data.get(data.size() - 1).timeStamp;
-            if (timestamp - lastTime < 10000000) {
+            if (timestamp - lastTime < 1000000) {
                 return;
             }
         }
@@ -70,19 +71,74 @@ public class SensorProcessor implements SensorEventListener {
         d.yOrientation = y;
         d.zOrientation = z;
         data.add(d);
-        while (data.size()> 0 && (timestamp - data.get(0).timeStamp) > 2000000000) { // reserve most recent 2s data
+        while (data.size()> 0 && (timestamp - data.get(0).timeStamp) > 500000000) { // reserve most recent 0.5s data
             data.remove(0);
         }
     }
     public enum RaiseStatus { Waiting, Fail, Prepare, Done };
     private RaiseStatus raiseStatus() {
-        if (data.size() < 10) { return RaiseStatus.Fail; }
-        double lastX = data.get(data.size() - 1).xOrientation;
-        for (int i= data.size() - 1; i>=0; i--) {
+        if (data.size() < 10) {
+            return RaiseStatus.Fail;
         }
-        System.out.println("data size: " + data.size());
 
-       return RaiseStatus.Fail;
+        SensorData currOri = data.get(data.size() - 1);
+
+        // Get speed on unit sphere within 200 ms
+        ArrayList<Double> speedLists = new ArrayList<>();
+        for (int i = 0; i + 1 < data.size(); i++) {
+            SensorData ori0 = data.get(i);
+            SensorData ori1 = data.get(i + 1);
+            if ((currOri.timeStamp - ori0.timeStamp) / 1e9 < 0.2) {
+                double escapeTime = (ori1.timeStamp - ori0.timeStamp) / 1e9;
+                double dX = (Math.cos(Math.toRadians(ori1.xOrientation))  - Math.cos(Math.toRadians(ori0.xOrientation))) / escapeTime;
+                double dY = (Math.cos(Math.toRadians(ori1.yOrientation))  - Math.cos(Math.toRadians(ori0.yOrientation))) / escapeTime;
+                double dZ = (Math.cos(Math.toRadians(ori1.zOrientation))  - Math.cos(Math.toRadians(ori0.zOrientation))) / escapeTime;
+                double speed = Math.sqrt(dX * dX + dY * dY + dZ * dZ);
+                speedLists.add(speed);
+            }
+        }
+
+        // Mid-num Filtering (window = 5)
+        for (int i = speedLists.size() - 1; i >= 4; i--) {
+            ArrayList<Double> sortList = new ArrayList<Double>(speedLists.subList(i - 4, i + 1));
+            Collections.sort(sortList);
+            double mid = sortList.get(sortList.size() / 2);
+            speedLists.set(i, mid);
+        }
+        // Mean-num Filtering (window = 5)
+        for (int i = speedLists.size() - 1; i >= 8; i--) {
+            double sum = 0;
+            for (int j = 0; j < 5; j++) {
+                sum += speedLists.get(i - j);
+            }
+            speedLists.set(i, sum / 5);
+        }
+        speedLists = new ArrayList<Double>(speedLists.subList(8, speedLists.size()));
+
+        // Calculate the 20% & 80% minimum speed
+        double lastSpeed = speedLists.get(speedLists.size() - 1);
+        Collections.sort(speedLists);
+        double minSpeed50 = speedLists.get((int)(speedLists.size() * 0.5));
+        double minSpeed90 = speedLists.get((int)(speedLists.size() * 0.9));
+
+        boolean check = true;
+        if (!(-15 <= currOri.xOrientation && currOri.xOrientation <= 15)) {
+            check = false;
+        }
+        if (!((45 <= currOri.yOrientation && currOri.yOrientation <= 135) || (-135 <= currOri.yOrientation && currOri.yOrientation <= -45))) {
+            check = false;
+        }
+
+        String output = "";
+        if (check && minSpeed50 >= 2 && minSpeed90 >= 4 && lastSpeed <= 2) {
+            //output = "x= " + (int)currOri.xOrientation + "\ny= " + (int)currOri.yOrientation + "\nSpeed50= " + minSpeed50 + "\nSpeed90= " + minSpeed90 + "\nLastSpeed= " + lastSpeed;
+            output = "Shoot!";
+        }
+
+        System.out.println(output);
+        mainActivity.showText(output);
+
+        return RaiseStatus.Fail;
     }
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
